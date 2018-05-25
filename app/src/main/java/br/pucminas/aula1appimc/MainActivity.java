6994df78ea0a658c06177e22538fdc9b666a666b
Karine Paz Fagundes Cordeiro <1082970@sga.pucminas.br>
2018-05-24 22:04:19 -0300

    first commit

---
+++ app/src/main/java/br/pucminas/aula1appimc/MainActivity.java
@@ -0,0 +1,13 @@
+package br.pucminas.aula1appimc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
